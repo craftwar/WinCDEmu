2e4006903f49e2de108a9af0845c45131f4a36e0
craftwar <craftwar@users.noreply.github.com>
2017-12-31 16:10:32 +0800

    3

--- path.cmd
+++ path.cmd
@@ -1,3 +1,7 @@
-set BZSLIB_PATH="C:\projects\wincdemu\BazisLib"
-set STLPORT_PATH="C:\projects\wincdemu\stlport-kernel"
-set WTL_PATH="C:\projects\wincdemu\wtl81_12085"
+set BZSLIB_PATH=C:\projects\wincdemu\BazisLib
+set STLPORT_PATH=C:\projects\wincdemu\stlport-kernel
+set WDK7PATH=C:\Program Files (x86)\Microsoft SDKs\Windows\v7.1A
+set WTL_PATH=C:\projects\wincdemu\wtl
+
+move BazisLib ..
+set BZSLIB_PATH=C:\projects\BazisLib
